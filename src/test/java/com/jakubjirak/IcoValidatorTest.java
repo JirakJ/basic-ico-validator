8a3b68d4203eeb2041884e200fd108876b69a92d
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-06-09 10:52:36 +0200

    Add test for ičo generator

--- src/test/java/com/jakubjirak/IcoValidatorTest.java
+++ src/test/java/com/jakubjirak/IcoValidatorTest.java
@@ -2,6 +2,8 @@ package com.jakubjirak;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,10 +12,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IcoValidatorTest {
     private IcoValidator icoValidator;
+    private IcoGenerator icoGenerator;
 
     @BeforeEach
     public void setUp() {
         icoValidator = new IcoValidator();
+        icoGenerator = new IcoGenerator();
     }
 
     @ParameterizedTest
@@ -28,5 +32,12 @@ public class IcoValidatorTest {
     @NullSource
     void checkNullInput(String ico) {
         assertTrue(icoValidator.check(ico));
+    }
+
+    @Test
+    @DisplayName("Check ICO generator that it returns always valid IČO")
+    @RepeatedTest(5000)
+    void checkIcoGenerator() {
+        assertTrue(icoValidator.check(icoGenerator.generate()));
     }
 }
